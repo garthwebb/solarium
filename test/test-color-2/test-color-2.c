9d5fe952a58f6a0fba75ce33812131f709af0bbc
Garth Webb <garth.webb@gmail.com>
2011-08-18 16:36:47 -0700

    Update this test to light the first row.

--- test/test-color-2/test-color-2.c
+++ test/test-color-2/test-color-2.c
@@ -4,9 +4,9 @@
 #include "i2c/device.h"
 #include <stdlib.h>
 
-#define ADDR_RED 4
-#define ADDR_GREEN 5
-#define ADDR_BLUE 2
+#define ADDR_RED 18 
+#define ADDR_GREEN 19
+#define ADDR_BLUE 20
 
 uint8_t red[16], green[16], blue[16];
 uint8_t max_val = 255;
@@ -16,6 +16,11 @@ void ooze (uint8_t *r, uint8_t *g, uint8_t *b);
 int main( int argc, char **argv ) {
 	init_devices();
 
+        int red_addr, grn_addr, blu_addr;
+        red_addr = atoi(argv[1]);
+        grn_addr = atoi(argv[2]);
+        blu_addr = atoi(argv[3]);
+
 	int i, r, g, b;
 
 	for (i=0; i<=15; i++) {
@@ -24,9 +29,9 @@ int main( int argc, char **argv ) {
 		blue[i] = 0;
 	}
 	for (i=REG_PWM0; i<=REG_PWM15; i++) {
-		write_register(ADDR_RED, i, 0);
-		write_register(ADDR_GREEN, i, 0);
-		write_register(ADDR_BLUE, i, 0);
+		write_register(red_addr, i, 0);
+		write_register(grn_addr, i, 0);
+		write_register(blu_addr, i, 0);
 	}
 
 	// Initial bu p values
@@ -76,18 +81,19 @@ int main( int argc, char **argv ) {
 		// For the rest ooze the color to the other pixels
 		ooze(red, green, blue);
 
-                fast_write_brightness (ADDR_RED, red, 16);
-                fast_write_brightness (ADDR_GREEN, green, 16);
-                fast_write_brightness (ADDR_BLUE, blue, 16);
+                fast_write_brightness (red_addr, red, 16);
+                fast_write_brightness (grn_addr, green, 16);
+                fast_write_brightness (blu_addr, blue, 16);
 //		for (i=REG_PWM0; i<=REG_PWM15; i++) {
 	//		if ((i < REG_PWM11) || (i == REG_PWM12) || (i == REG_PWM13) || (i == REG_PWM14) || (i == REG_PWM15)) {
-//			write_register(ADDR_RED, i, red[i-REG_PWM0]);
-//			write_register(ADDR_GREEN, i, green[i-REG_PWM0]);
-//			write_register(ADDR_BLUE, i, blue[i-REG_PWM0]);
+//			write_register(red_addr, i, red[i-REG_PWM0]);
+//			write_register(grn_addr, i, green[i-REG_PWM0]);
+//			write_register(blu_addr, i, blue[i-REG_PWM0]);
 	//		}
 //		}
 	}
 
+	fprintf(stderr, "Exiting...\n");
 	cleanup();
 }
 
@@ -101,3 +107,4 @@ void ooze (uint8_t *r, uint8_t *g, uint8_t *b) {
 		b[i] = b[i-1];
 	}
 }
+
