4bd4f1ec4edf4b1c997c6791c5ca29b755662e1a
Bryan Beeley <b@beeley.org>
2011-08-24 01:37:35 -0700

    Incorporate the new color map

--- test/sun-test/sun-test.c
+++ test/sun-test/sun-test.c
@@ -31,11 +31,13 @@ int main (void)
 */
 	color_t *color_map = get_color_map(0);
 
+/*
 	for (i = 0; i < COLOR_MAP_SIZE; i++) {
 		color_map[i].red = 0;
 		color_map[i].green = 10;
 		color_map[i].blue = 0;
 	}
+*/
 
 	double radians[360];
 
